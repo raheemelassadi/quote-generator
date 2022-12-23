464858b8e3bc7ecff0dead4bf50a403b79a946f1
raheemelassadi <raheemelassadi1@gmail.com>
2022-12-23 02:40:58 -0800

    added styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
 import { useState } from 'react'
-import './App.css'
+import './App.scss'
 
 function App() {
 
 
-  const quote = () => {
+  const getQuote = () => {
     const randomNumber = Math.floor(Math.random() * 1500)
     fetch("https://type.fit/api/quotes")
       .then(response => response.json())
       .then(data => setDisplay(data[randomNumber]))
   }
 
-  const [display, setDisplay] = useState([])
+  const [display, setDisplay] = useState('')
 
   return (
-    <div className="App">
-      <button onClick={quote}>get quote</button>
-      <div>{display.text}</div>
-      <div>{display.author}</div>
+    <div className='App'>
+      <div className='button'>
+        <button onClick={getQuote}>Get Quote</button>
+      </div>
+      <div className='content'>
+        <div id='quote'>"{display.text}"</div>
+        <div id='author'>-{display.author}</div>
+      </div>
     </div>
   )
 }
