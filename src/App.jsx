bb6e0c16a60001ec1a9dc6a7403d20006a8c6e40
raheemelassadi <raheemelassadi1@gmail.com>
2022-12-27 04:48:18 -0800

    added react spinner loader and customized css for it

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.scss'
+import HashLoader from "react-spinners/HashLoader";
+
 
 function App() {
 
+  const [display, setDisplay] = useState('')
+
+  const [loading, setLoading] = useState(false)
 
   const getQuote = () => {
     const randomNumber = Math.floor(Math.random() * 1500)
@@ -11,10 +16,33 @@ function App() {
       .then(data => setDisplay(data[randomNumber]))
   }
 
-  const [display, setDisplay] = useState('')
+  useEffect(() => {
+    const randomNumber = Math.floor(Math.random() * 1500)
+    fetch("https://type.fit/api/quotes")
+      .then(response => response.json())
+      .then(data => setDisplay(data[randomNumber]))
+  }, [])
+
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 2500)
+  }, [])
 
   return (
+    <div className='main'>
+      {
+        loading ?  
+        <div id='loader'>  
+        <HashLoader
+        color="#36d7b7"
+        cssOverride={{}}
+      /></div>
+      :
+      
     <div className='App'>
+      
       <div className='button'>
         <button onClick={getQuote}>Get Quote</button>
       </div>
@@ -22,6 +50,8 @@ function App() {
         <div id='quote'>"{display.text}"</div>
         <div id='author'>-{display.author}</div>
       </div>
+    </div>
+    }
     </div>
   )
 }
